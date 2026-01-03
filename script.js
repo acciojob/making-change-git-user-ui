cc7ef989f0be5d4c92a707756964f9e6c7b077ef
git-user-ui <pratikpp2302@gmail.com>
2026-01-03 21:14:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 const makeChange = (c) => {
-  // your name here
+  c = Number(c); // prompt gives string
+
+  const result = {
+    q: 0,
+    d: 0,
+    n: 0,
+    p: 0
+  };
+
+  // Quarters
+  result.q = Math.floor(c / 25);
+  c = c % 25;
+
+  // Dimes
+  result.d = Math.floor(c / 10);
+  c = c % 10;
+
+  // Nickels
+  result.n = Math.floor(c / 5);
+  c = c % 5;
+
+  // Pennies
+  result.p = c;
+
+  return result;
 };
 
 // Do not the change the code below
